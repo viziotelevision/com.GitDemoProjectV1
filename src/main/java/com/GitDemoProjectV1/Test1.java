5ae5971029495e7a018327c1a41af4e00df24245
sunil <sunil@LAPTOP-3V6AOGNK>
2020-01-05 13:52:27 -0600

    First commit - Lead

---
+++ src/main/java/com/GitDemoProjectV1/Test1.java
@@ -0,0 +1,16 @@
+package com.GitDemoProjectV1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+
+		if (hungry == "yes")
+			System.out.println("eat something ....");
+		else
+			System.out.println("Do work ....");
+
+	}
+
+}
