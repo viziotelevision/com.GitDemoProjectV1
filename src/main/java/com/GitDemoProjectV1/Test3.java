a5088783ef11d79a114bf2555f9d703391cfd53f
viziotelevision <viziotelevision@LAPTOP-3V6AOGNK>
2020-01-05 15:48:52 -0600

    Test2 has been updated and Test3 has been added by Lead

--- src/main/java/com/GitDemoProjectV1/Test3.java
+++ src/main/java/com/GitDemoProjectV1/Test3.java
@@ -1,10 +1,9 @@
 package com.GitDemoProjectV1;
 
-public class Test2 {
-
+public class Test3 {
 	public static void main(String[] args) {
 
-		String hungry = "no";
+		String hungry = "yes";
 
 		if (hungry == "yes")
 			System.out.println("eat something ....");
