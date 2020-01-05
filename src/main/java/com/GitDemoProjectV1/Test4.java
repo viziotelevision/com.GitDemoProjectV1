4330dfa1f0d65e308123d6c6f6205f89c9419707
viziotelevision <viziotelevision@LAPTOP-3V6AOGNK>
2020-01-05 15:57:56 -0600

    Test4 has been added by Dev

---
+++ src/main/java/com/GitDemoProjectV1/Test4.java
@@ -0,0 +1,15 @@
+package com.GitDemoProjectV1;
+
+public class Test4 {
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+
+		if (hungry == "yes" || hungry=="Y" || hungry=="y")
+			System.out.println("eat something ....");
+		else
+			System.out.println("Do work ....");
+
+	}
+
+}
