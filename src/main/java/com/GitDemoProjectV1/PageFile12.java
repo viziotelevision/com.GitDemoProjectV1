2153cbad98752d69b48be813d60036cbef563641
viziotelevision <57381559+viziotelevision@users.noreply.github.com>
2021-04-11 12:12:10 -0500

    This is my PageFile12 - created in MasterInRemote

---
+++ src/main/java/com/GitDemoProjectV1/PageFile12.java
@@ -0,0 +1,10 @@
+package com.GitDemoProjectV1;
+
+public class PageFile12 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my PageFile12 - created in MasterInRemote");
+	
+	}
+
+}
