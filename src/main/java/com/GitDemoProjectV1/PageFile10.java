3a8f747f299e491a3e68b1898dd466867bc1b435
viziotelevision <57381559+viziotelevision@users.noreply.github.com>
2021-04-11 11:34:18 -0500

    Remote - Master - New file PageFile10 added

---
+++ src/main/java/com/GitDemoProjectV1/PageFile10.java
@@ -0,0 +1,10 @@
+package com.GitDemoProjectV1;
+
+public class PageFile10 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my PageFile10 - created in MasterInRemote");
+	
+	}
+
+}
